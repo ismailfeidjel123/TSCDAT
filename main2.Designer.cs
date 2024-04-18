6ee2d18230073fc203d484e2b2a7e40044669060
pc8 <pc8@HFPRO>
2024-04-18 18:02:06 +0100

    fgfg

--- main2.Designer.cs
+++ main2.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnstart = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.btnstop = new Guna.UI2.WinForms.Guna2Button();
+            this.cboWebcams = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.pictureBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.lblObjectCount = new System.Windows.Forms.Label();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.lblRAM = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnstart
+            // 
+            this.btnstart.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnstart.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnstart.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnstart.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnstart.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnstart.ForeColor = System.Drawing.Color.White;
+            this.btnstart.Location = new System.Drawing.Point(51, 105);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(180, 45);
+            this.btnstart.TabIndex = 0;
+            this.btnstart.Text = "START";
+            this.btnstart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(51, 185);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "START COUNTING";
+            // 
+            // btnstop
+            // 
+            this.btnstop.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnstop.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnstop.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnstop.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnstop.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnstop.ForeColor = System.Drawing.Color.White;
+            this.btnstop.Location = new System.Drawing.Point(51, 266);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(180, 45);
+            this.btnstop.TabIndex = 2;
+            this.btnstop.Text = "STOP";
+            this.btnstop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cboWebcams
+            // 
+            this.cboWebcams.BackColor = System.Drawing.Color.Transparent;
+            this.cboWebcams.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cboWebcams.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboWebcams.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cboWebcams.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cboWebcams.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cboWebcams.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cboWebcams.ItemHeight = 30;
+            this.cboWebcams.Location = new System.Drawing.Point(51, 48);
+            this.cboWebcams.Name = "cboWebcams";
+            this.cboWebcams.Size = new System.Drawing.Size(180, 36);
+            this.cboWebcams.TabIndex = 3;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.ImageRotate = 0F;
+            this.pictureBox.Location = new System.Drawing.Point(237, 48);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(349, 263);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(592, 48);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(349, 263);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox2.TabIndex = 5;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // lblObjectCount
+            // 
+            this.lblObjectCount.AutoSize = true;
+            this.lblObjectCount.Location = new System.Drawing.Point(48, 334);
+            this.lblObjectCount.Name = "lblObjectCount";
+            this.lblObjectCount.Size = new System.Drawing.Size(29, 13);
+            this.lblObjectCount.TabIndex = 6;
+            this.lblObjectCount.Text = "OVC";
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Location = new System.Drawing.Point(48, 358);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(39, 13);
+            this.lblCPU.TabIndex = 7;
+            this.lblCPU.Text = "lblCPU";
+            // 
+            // lblRAM
+            // 
+            this.lblRAM.AutoSize = true;
+            this.lblRAM.Location = new System.Drawing.Point(48, 382);
+            this.lblRAM.Name = "lblRAM";
+            this.lblRAM.Size = new System.Drawing.Size(39, 13);
+            this.lblRAM.TabIndex = 8;
+            this.lblRAM.Text = "lblCPU";
+            // 
+            // main2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 518);
+            this.Controls.Add(this.lblRAM);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.lblObjectCount);
+            this.Controls.Add(this.guna2PictureBox2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.cboWebcams);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.btnstart);
+            this.Name = "main2";
             this.Text = "main2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnstart;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button btnstop;
+        private Guna.UI2.WinForms.Guna2ComboBox cboWebcams;
+        private Guna.UI2.WinForms.Guna2PictureBox pictureBox;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private System.Windows.Forms.Label lblObjectCount;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label lblRAM;
     }
 }
--- main2.cs
+++ main2.cs
@@ -1,20 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
 
 namespace MultiFaceRec
 {
     public partial class main2 : Form
     {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private int objectCount = 0;
+
         public main2()
         {
             InitializeComponent();
+            InitializeWebcams();
+        }
+
+        private void InitializeWebcams()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in videoDevices)
+            {
+                cboWebcams.Items.Add(device.Name);
+            }
+
+            if (cboWebcams.Items.Count > 0)
+            {
+                cboWebcams.SelectedIndex = 0;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (videoDevices != null && videoDevices.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[cboWebcams.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+
+                // Clear the PictureBox
+                pictureBox.Image = null;
+            }
+        }
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            // Apply image processing to detect objects
+            int count = CountObjects(bitmap);
+
+            // Display the result
+            UpdateObjectCount(count);
+            pictureBox.Image = bitmap;
+
+            // Display CPU and RAM usage
+            DisplayPerformanceInfo();
+        }
+
+        private int CountObjects(Bitmap bitmap)
+        {
+            // Implement object detection here
+            return 0; // Placeholder
+        }
+
+        private void UpdateObjectCount(int count)
+        {
+            // Update the object count label
+            lblObjectCount.Invoke((MethodInvoker)delegate {
+                lblObjectCount.Text = "Object Count: " + count.ToString();
+            });
+        }
+
+        private void DisplayPerformanceInfo()
+        {
+            // Get CPU usage percentage
+            float cpuUsage = GetCPUUsage();
+            lblCPU.Invoke((MethodInvoker)delegate {
+                lblCPU.Text = "CPU Usage: " + cpuUsage.ToString("0.00") + "%";
+            });
+
+            // Get RAM usage percentage
+            float ramUsage = GetRAMUsage();
+            lblRAM.Invoke((MethodInvoker)delegate {
+                lblRAM.Text = "RAM Usage: " + ramUsage.ToString("0.00") + "%";
+            });
+        }
+
+        private float GetCPUUsage()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            cpuCounter.NextValue(); // Call this first to get the initial value
+            System.Threading.Thread.Sleep(500); // Wait a short moment for the value to be updated
+            return cpuCounter.NextValue();
+        }
+
+        private float GetRAMUsage()
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            return ramCounter.NextValue();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
         }
     }
 }
